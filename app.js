f47b424ee4501a719f505edae89df6d9aee40b2e
jiangzhibin <978692770@qq.com>
2018-08-26 09:37:57 +0800

    提交博客

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('Hello Blog');
+});
+
+app.listen(3000, err => {
+    if (err == null) {
+        console.log('服务器启动成功,请访问http://localhost:3000');
+    }
+})
